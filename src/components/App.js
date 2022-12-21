e33664da469129b58862ce02d3ab8a2e708af959
ilona <gogiashvili.ilona@gmail.com>
2022-12-21 11:41:29 -0500

    commit tree f7b7d4c5cb75aa9ba35746ebbca9fa886a18e72e

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
 
 function App() {
+  const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  function onDarkModeClick() {
+  const onCategoryChange = (id) => {
+    const newItems = items.filter(item => item.id !== id)
+    setItems(newItems)
+  }
+
+  function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
+      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+      <ShoppingList items={items} onCategoryChange={onCategoryChange} />
     </div>
   );
 }
